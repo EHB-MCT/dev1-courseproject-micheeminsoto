eedcbbacf6bb6fc5aa7692cd1756ce6ffd94a7a6
michee minsoto <micheeminsoto@student.ehb.be>
2024-10-02 13:12:32 +0200

    black background

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,19 @@
 "use strict";
 
-alert("Draw your space invader here");
+/* alert("Draw your space invader here"); */
+
+drawRectangle();
+drawSpaceInvader();
+
+function drawRectangle() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+	
+
+
+	context.rect(0, 0, canvas.width,canvas.height);
+	context.fillStyle = "#000000";
+	context.fill();
+}
