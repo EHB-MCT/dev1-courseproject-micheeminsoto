a8b9eab50ebb7dba1e7f29ed7368aa24c8caf140
michee minsoto <micheeminsoto@student.ehb.be>
2024-10-02 19:40:14 +0200

    black rectangle canvas

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,27 +1,62 @@
 "use strict";
 
+
 /* alert("Draw your space invader here"); */
 
-drawRectangle();
-drawSpaceInvader();
 
-function drawRectangle() {
-	let canvas = document.querySelector("canvas");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	let context = canvas.getContext("2d");
-	
-	context.rect(0, 0, canvas.width,canvas.height);
-	context.fillStyle = "#000000";
-	context.fill();
+setup();
+window.onload = setup;
+
+
+
+function setup() {
+	drawRectangle(); 
+	drawSpaceInvader();
+	window.onresize = setup;
 }
 
-function drawSpaceInvader() {
+
+ function drawRectangle() {
 	let canvas = document.querySelector("canvas");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	let context = canvas.getContext("2d");
+	
+	
+	context.rect(0, 0,canvas.width,canvas.height);
+	context.fillStyle = "#000000";
+	context.fill();
+} 
+ 
+function drawSpaceInvader() {
+	let canvas = document.querySelector("canvas");
+	let context = canvas.getContext("2d");
+	
+	
+	context.beginPath();
+	context.fillRect(50,50,50,50);
+	context.fillStyle = "#00ff00";
+	context.fill();
 
 	context.beginPath();
+	context.fillRect(700,200,50,50);
+	context.fillStyle = "##00ff00";
+	context.fill();
+
+	context.beginPath();
+	context.fillRect(600,200,50,50);
+	context.fillStyle = "#00ff00";
+	context.fill();
+
+	context.beginPath();
+	context.fillRect(600,300,150,100);
+	context.fill();
+
+	context.beginPath();
+	context.fillRect(650,250,50,50);
+	context.fill(); 
+
+
 	
+
 }
