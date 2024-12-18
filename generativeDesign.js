a78bc571c8b601fb04fb22d9b6e250c34240389d
michee minsoto <micheeminsoto@student.ehb.be>
2024-12-18 12:44:16 +0100

    TAAK KLAAR MICHEE MINSOTO

--- generativeDesign.js
+++ generativeDesign.js
@@ -3,6 +3,7 @@ import context from "./scripts/context.js";
 import * as Utils from "./scripts/utils.js";
 
 let canvas = document.createElement("canvas");
+document.body.appendChild(canvas); // Voeg het canvas toe aan de body
 
 
 
